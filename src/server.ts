db017489e917811a5666fc637e17ba6054ce42bd
Ivan Lara <ukikalix@gmail.com>
2025-03-29 02:01:26 -0400

    fx/cors config added

--- src/server.ts
+++ src/server.ts
@@ -11,16 +11,21 @@ import { swaggerSpec } from "./../swagger";
 import authRoutes from "./routes/auth/auth.route";
 import taskRoutes from "./routes/task/task.route";
 
+const CORS_CONFIG = {
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+};
+
 const app: Application = express();
 app.use(express.json());
-app.use("/docs", cors(), swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(
-    cors({
-        origin: "*",
-        methods: "GET, POST, PUT, DELETE",
-        allowedHeaders: "Content-Type, Authorization",
-    })
+    "/docs",
+    cors(CORS_CONFIG),
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec)
 );
+app.use(cors(CORS_CONFIG));
 
 // Use the router to add prefix for all routes
 const router = express.Router();
